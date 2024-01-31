a5112bb044cca5a7f6a693a0f7eb64f903d0f4d7
Apple <a.karna@inspheris.com>
2024-01-31 23:07:41 +0545

    Fix: added branch name

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["EKYouTubeExtractor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ashish7-iOS/Lively-EKYouTubeExtractor.git", from: "1.0.1"),
+        .package(url: "https://github.com/ashish7-iOS/Lively-EKYouTubeExtractor.git", .branch("develop")),
     ],
     targets: [
         .target(
